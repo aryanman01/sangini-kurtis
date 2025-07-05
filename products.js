fb0d241f9ba7a1f92a6b9cf5c28b0c9ef3f3d526
aryanman01 <aryanman097@gmail.com>
2025-07-05 11:29:00 +0530

    add images and wapp

--- products.js
+++ products.js
@@ -4,25 +4,25 @@ const products = [
     category: "Kurtis",
     name: "Floral Printed Kurti",
     price: "₹799",
-    image: "images/sample_kurti.jpg"
+    image: "images/sample_kurti1.jpeg"
   },
   {
     category: "Plazzo",
     name: "Elegant Plazzo Pants",
     price: "₹599",
-    image: "images/sample_plazzo.jpg"
+    image: "images/sample_plazzo.jpeg"
   },
   {
     category: "Co-ord Sets",
     name: "Stylish Co-ord Set",
     price: "₹1299",
-    image: "images/sample_coord.jpg"
+    image: "images/sample_coord.jpeg"
   },
   {
     category: "Leggings",
     name: "Comfort Fit Leggings",
     price: "₹399",
-    image: "images/sample_leggings.jpg"
+    image: "images/sample_leggings.jpeg"
   }
 ];
 
@@ -40,7 +40,7 @@ products.forEach(product => {
     <p>Price: ${product.price}</p>
     <a
       class="order-button"
-      href="https://wa.me/YOUR_NUMBER?text=Hi%20I%20want%20to%20order%20${encodeURIComponent(product.name)}%20from%20Sangini%20Kurtis"
+      href="https://wa.me/7008697514?text=Hi%20I%20want%20to%20order%20${encodeURIComponent(product.name)}%20from%20Sangini%20Kurtis"
       target="_blank"
     >Order on WhatsApp</a>
   `;
