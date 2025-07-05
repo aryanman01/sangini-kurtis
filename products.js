e57101d38c4b03618151f568c8f7588feadd0a6e
aryanman01 <aryanman097@gmail.com>
2025-07-05 12:00:04 +0530

    update name

--- products.js
+++ products.js
@@ -1,4 +1,4 @@
-// Sample product data for Sangini Kurtis
+// Sample product data for Sunidhi Designer Kurtis
 const products = [
   {
     category: "Kurtis",
@@ -40,7 +40,7 @@ products.forEach(product => {
     <p>Price: ${product.price}</p>
     <a
       class="order-button"
-      href="https://wa.me/7991183180?text=Hi%20I%20want%20to%20order%20${encodeURIComponent(product.name)}%20from%20Sangini%20Kurtis"
+      href="https://wa.me/7991183180?text=Hi%20I%20want%20to%20order%20${encodeURIComponent(product.name)}%20from%20Sunidhi%20Designer%20Kurtis"
       target="_blank"
     >Order on WhatsApp</a>
   `;
